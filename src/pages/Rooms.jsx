cf4471c5c321f34a3927b59ae0cd8683cc928005
goncalhurtado <gonzalomilano@gmail.com>
2023-11-06 18:35:54 -0300

    added json server

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosInstance } from "../config/axiosInstance";
 
 const Rooms = () => {
+  const [rooms, setRooms] = useState({});
+
+  const getRooms = async () => {
+    try {
+      const response = await axiosInstance.get("/categories");
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getRooms();
+  }, []);
+
   return <div>Rooms</div>;
 };
 
