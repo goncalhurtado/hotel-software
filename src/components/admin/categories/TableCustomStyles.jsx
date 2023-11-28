7368d328a0b53973834db55598790ec4040b03ef
goncalhurtado <gonzalomilano@gmail.com>
2023-11-28 00:54:22 -0300

    added styles to category table

---
+++ src/components/admin/categories/TableCustomStyles.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const customStyles = {
+  rows: {
+    style: {
+      fontFamily: "Roboto, Helvetica, Arial, sans-serif;",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "16px",
+    },
+  },
+  headCells: {
+    style: {
+      display: "flex",
+      justifyContent: "start",
+      paddingLeft: "4px",
+      paddingRight: "5px",
+      border: "2px solid #F1F1F1",
+      fontFamily: "Roboto, Helvetica, Arial, sans-serif;",
+      fontWeight: "bold",
+      fontSize: "15px",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  cells: {
+    style: {
+      minHeight: "20px",
+      paddingLeft: "4px",
+      paddingRight: "5px",
+      borderRight: "2px solid #F1F1F1",
+      borderLeft: "2px solid #F1F1F1",
+    },
+  },
+};
+
+export { customStyles };
