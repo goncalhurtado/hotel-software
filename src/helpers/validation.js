6ed4ee63b69754b75815c5abff609d156d461c1d
goncalhurtado <gonzalomilano@gmail.com>
2023-12-10 20:00:31 -0300

    added validation to booking form comp

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,13 +1,27 @@
 export const validate = (formData, setError) => {
 
-    const { firstName, lastName, email, phone, country, passportType, passport, arrivalTime, paymentMethod, additionalComments } = formData;
-
+    const { firstName, lastName, email, phone, country, passportType, passport, arrivalTime, paymentMethod } = formData.info;
 
     if (!firstName || !lastName || !email || !phone || !country || !passportType || !passport || !arrivalTime || !paymentMethod) {
         setError({ status: true, message: 'Please fill all the required fields.' });
         return false;
     }
 
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-    console.log(formData);
+    if (!emailRegex.test(email)) {
+        setError({ status: true, message: 'Please enter a valid email address.' });
+        return false;
+    }
+
+    const phoneRegex = /^\d{1,20}$/;
+
+    if (!phoneRegex.test(phone)) {
+        setError({ status: true, message: 'Please enter a valid phone number without special characters.' });
+
+        return false;
+    }
+
+    return true;
+
 }
